094c1c39ac1fe6762f9aa910b5a73ef87c1becf6
Howard Xue <howard_xue@hotmail.com>
2014-02-09 15:00:04 -0800

    basic setup

---
+++ application.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	var questionArr = [question1, question2, question3, question4, question5];
+	var question1 = {question: "What is 1+1?", choices: ["1", "11", "2", "-485"], correct: 2};
+	var question2 = {question: "What's 2 x 2?", choices: ["4", "6", "12", "22"], correct: 0};
+
+	$("#question").append("<p>What is this?</p>");
+	
+});
